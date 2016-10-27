0117938dd8c8ac47d71959060bcbd804aa7d5af8
Stargateur <antoine.plaskowski@epitech.eu>
2016-10-27 20:42:38 +0200

    git clone in replace of git submodule

--- build.rs
+++ build.rs
@@ -1,13 +1,18 @@
 extern crate cmake;
+extern crate git2 as git;
+
 
 fn main() {
-    let dst = cmake::Config::new("librtmp").build_target("rtmp").build();
+    let dir = std::env::var("OUT_DIR").unwrap() + "/librtmp";
+    let url = "https://github.com/Stargateur/librtmp";
+    let _ = git::Repository::clone(url, &dir);
+    let dst = cmake::Config::new(&dir).build_target("rtmp").build();
 
     if cfg!(any(windows)) {
-        println!("cargo:rustc-link-search=native={}/build/Release", dst.display());
+        println!("cargo:rustc-link-search=native={}/build/Release", dst.display())
     }
     else {
-        println!("cargo:rustc-link-search=native={}/build", dst.display());
+        println!("cargo:rustc-link-search=native={}/build", dst.display())
     }
-    println!("cargo:rustc-link-lib=static=rtmp");
+    println!("cargo:rustc-link-lib=static=rtmp")
 }
